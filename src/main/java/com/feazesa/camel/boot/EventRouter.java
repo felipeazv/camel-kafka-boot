9210c4d5a465b7a0246438f9f9dd20c2a6354b9d
felipeazv <felipe.azv@gmail.com>
2018-07-31 22:37:19 +0200

    added support for netty4:tcp processing

--- src/main/java/com/feazesa/camel/boot/EventRouter.java
+++ src/main/java/com/feazesa/camel/boot/EventRouter.java
@@ -1,5 +1,7 @@
 package com.feazesa.camel.boot;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -19,10 +21,23 @@ public class EventRouter extends RouteBuilder {
                 .end()
                 .to("stream:out");
         */
+
+        //kafka to log
+
         from("kafka:camelTopic?brokers=192.168.99.100:9092&groupId=camelKafka")
                 .to("log:kafka")
                 .end()
                 .to("stream:out");
+
+        //TCP (netty4) to log
+        from("netty4:tcp://localhost:8081?keepAlive=true&textline=true&sync=true")
+                .routeId("netty4tcptest")
+                .routeGroup("netty4tcptest-group")
+                .to("log:netty4tcptest")
+                .end()
+                .to("stream:out");
+
     }
+
 
 }
